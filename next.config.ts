1f3df2fb56bb6272994d43580f20fdb31f1ae544
sarahqii <yueran79@gmail.com>
2025-08-12 22:04:59 +0800

    Modify deploy setting for new domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  output: 'export',
+  assetPrefix: './',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
